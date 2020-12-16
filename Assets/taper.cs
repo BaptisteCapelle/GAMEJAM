856dd6ee5b724d0270e160170815d224bafba3c7
LAPTOP-FIUKPBFK\Baptiste.C <baptiste.capelle@lecole-ldlc.com>
2020-12-16 12:45:36 +0100

    V2 taper

---
+++ Assets/taper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class taper : MonoBehaviour
+{
+    [SerializeField]
+    float radius = 1f;
+    [SerializeField]
+    float radiusExplosion = 1f;
+    
+    [SerializeField]
+    float distance = 10f;
+    [SerializeField]
+    public float power = 10.0F;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //(Vector3 origin, float radius, Vector3 direction, out RaycastHit hitInfo, float maxDistance = Mathf.Infinity
+            RaycastHit[] hitResults = Physics.SphereCastAll(transform.position + transform.forward * 1f, radius, transform.forward, distance);
+
+            if (hitResults != null && hitResults.Length > 0)
+            {
+
+                foreach (RaycastHit resultat in hitResults)
+                {
+                
+                    if (resultat.transform.gameObject.CompareTag("Finish"))
+                    {
+                        Debug.Log(resultat.transform.name);
+                        resultat.rigidbody.AddExplosionForce(power, resultat.point, radiusExplosion, 3.0F);
+                        //resultat.rigidbody 
+                    }
+                }
+            }
+        }
+    }
+
+}
+
