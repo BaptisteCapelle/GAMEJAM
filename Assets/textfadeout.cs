b7144fe51f9a9189e2ef344f6287cb42c2b7cb3d
LAPTOP-FIUKPBFK\Baptiste.C <baptiste.capelle@lecole-ldlc.com>
2020-12-17 14:36:19 +0100

    V2

---
+++ Assets/textfadeout.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textfadeout : MonoBehaviour
+{
+    public float lifetime = 13;
+    public void Start()
+    {
+        Destroy(this.gameObject, lifetime);
+    }
+}
