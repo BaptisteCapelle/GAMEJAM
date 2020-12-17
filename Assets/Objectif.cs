011a9de63b2ad80edc3e5830934e40020004032d
LAPTOP-FIUKPBFK\Baptiste.C <baptiste.capelle@lecole-ldlc.com>
2020-12-17 10:49:16 +0100

    OBJECTIFS + MUSIQUE AMBIANCE

---
+++ Assets/Objectif.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objectif : Actions
+{
+    public AudioClip cadre;
+    protected override void ExecuteAction(RaycastHit resultat)
+    {
+        Debug.Log(resultat.transform.name);
+        AudioSource.PlayClipAtPoint(cadre, gameObject.transform.position);
+        resultat.rigidbody.gameObject.SetActive(false);
+        objectif5 = objectif5 + 1;
+
+        if (objectif5 == 5)
+        {
+            Debug.Log("GAGNE");
+        }
+
+    }
+
+    int objectif5;
+    protected override bool GetActionKey()
+    {
+        if (Input.GetKeyDown(KeyCode.E) == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
